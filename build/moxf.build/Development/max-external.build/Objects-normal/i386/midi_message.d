d37a607294e67ea9ab6bfaccdcfcd1cd63641009
James Hook <jameshook@Jamess-Mac-mini.local>
2018-03-03 13:29:22 +0000

    added git ignore

--- build/moxf.build/Development/max-external.build/Objects-normal/i386/midi_message.d
+++
@@ -1,7 +0,0 @@
-dependencies: \
-  /Users/jameshook/dev/src/max-sdk-7.3.3/source/moxf/moxf/midi/midi_message.cpp \
-  /Users/jameshook/dev/src/max-sdk-7.3.3/source/moxf/moxf/../../c74support/max-includes/macho-prefix.pch \
-  /Users/jameshook/dev/src/max-sdk-7.3.3/source/moxf/moxf/midi/midi_message.h \
-  /Users/jameshook/dev/src/max-sdk-7.3.3/source/moxf/moxf/midi/moxf_types.h \
-  /Users/jameshook/dev/src/max-sdk-7.3.3/source/moxf/moxf/midi/rtmidi_utils.hpp \
-  /usr/local/include/rtmidi/RtMidi.h
